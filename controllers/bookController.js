aba54b404c08a43d645f0aa355a22fbc26d33cc2
Edmilson Miyasaki <edmilson.miyasaki@ilegra.com>
2016-01-11 17:07:33 -0200

    finishing separating code

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -25,9 +25,59 @@ var bookController = function(Book) {
 		});
 	};
 
+	var getById = function(req, res) {
+		res.json(req.book);
+	};
+
+	var putById = function(req, res) {
+
+		Book.findById(req.params.bookId, function(err, book) {
+			if (err)
+				res.status(500).send(err);
+			else {
+				req.book.title = req.body.title;
+				req.book.author = req.body.author;
+				req.book.genre = req.body.genre;
+				req.book.read = req.body.read;
+				req.book.save(function(err) {
+					res.json(req.book);
+				});
+			}
+		});
+	};
+
+	var patchById = function(req, res) {
+		if (req.body._id) {
+			delete req.body._id;
+		}
+		for (var p in req.body) {
+			req.book[p] = req.body[p];
+		}
+		req.book.save(function(err) {
+			if (err) {
+				res.status(500).send(err);
+			} else {
+				res.json(req.book);
+			}
+		});
+	};
+	var deleteById = function(req, res) {
+		req.book.remove(function(err) {
+			if (err) {
+				res.status(500).send(err);
+			} else {
+				res.status(204).send('Book removed!');
+			}
+		});
+	};
+
 	return {
 		post: post,
-		get: get
+		get: get,
+		getById: getById,
+		putById: putById,
+		patchById: patchById,
+		deleteById: deleteById
 	};
 };
 
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -22,49 +22,10 @@ var routes = function(Book) {
 	});
 
 	bookRouter.route('/:bookId')
-		.get(function(req, res) {
-			res.json(req.book);
-		})
-		.put(function(req, res) {
-
-			Book.findById(req.params.bookId, function(err, book) {
-				if (err)
-					res.status(500).send(err);
-				else {
-					req.book.title = req.body.title;
-					req.book.author = req.body.author;
-					req.book.genre = req.body.genre;
-					req.book.read = req.body.read;
-					req.book.save(function(err) {
-						res.json(req.book);
-					});
-				}
-			});
-		})
-		.patch(function(req, res) {
-			if (req.body._id) {
-				delete req.body._id;
-			}
-			for (var p in req.body) {
-				req.book[p] = req.body[p];
-			}
-			req.book.save(function(err) {
-				if (err) {
-					res.status(500).send(err);
-				} else {
-					res.json(req.book);
-				}
-			});
-		})
-		.delete(function(req, res){
-			req.book.remove(function(err){
-				if(err){
-					res.status(500).send(err);
-				} else {
-					res.status(204).send('Book removed!');
-				}
-			});
-		});
+		.get(bookController.getById)
+		.put(bookController.putById)
+		.patch(bookController.patchById)
+		.delete(bookController.deleteById);
 
 	return bookRouter;
 };
